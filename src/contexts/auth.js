6f27b6ff0556177714b0df9f10ddab12ab63f906
g-freitas <gabriel.ff@outlook.com>
2024-06-01 14:36:47 -0300

    Creating private pages and log out button

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,6 +1,6 @@
 import { useState, createContext, useEffect } from 'react';
 import { auth, db } from '../services/firebaseConnection';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import { doc, getDoc, setDoc } from 'firebase/firestore'
 
 import { useNavigate } from 'react-router-dom';
@@ -11,8 +11,27 @@ export const AuthContext = createContext({});
 function AuthProvider({ children }){
   const [user, setUser] = useState(null)
   const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const navigate = useNavigate();
+
+
+  useEffect(() => {
+    async function loadUser(){
+        const storageUser = localStorage.getItem('@ticketsPRO')
+
+        if(storageUser){
+            setUser(JSON.parse(storageUser))
+            setLoading(false);
+        }
+
+
+        setLoading(false);
+
+    }
+
+    loadUser();
+  }, [])
 
   async function signIn(email, password){
     setLoadingAuth(true);
@@ -85,7 +104,13 @@ function AuthProvider({ children }){
 
 
   function storageUser(data){
-    localStorage.setItem('@tickestsPRO', JSON.stringify(data))
+    localStorage.setItem('@ticketsPRO', JSON.stringify(data))
+  }
+
+  async function logout(){
+    await signOut(auth);
+    localStorage.removeItem('@ticketsPRO');
+    setUser(null);
   }
 
   return(
@@ -95,7 +120,9 @@ function AuthProvider({ children }){
         user,
         signIn,
         signUp,
-        loadingAuth
+        logout,
+        loadingAuth,
+        loading
       }}
     >
       {children}
