9c62d757a73078028e8fed1a6b90bf2a7ff8aa83
g-freitas <gabriel.ff@outlook.com>
2024-06-15 16:47:15 -0300

    Updating Perfil

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -122,7 +122,9 @@ function AuthProvider({ children }){
         signUp,
         logout,
         loadingAuth,
-        loading
+        loading,
+        storageUser,
+        setUser
       }}
     >
       {children}
